24bf048154da70262eba6fc26b9b5d15daa58e7c
Bronson Schultz <bcs269@peon11.usask.ca>
2020-02-03 19:28:10 -0600

    created json to show in browser

--- a2.js
+++ a2.js
@@ -1,10 +1,16 @@
+
+const express = require('express');
 const http = require('http');
 const sqlite3 = require('sqlite3');
-const hostname = '127.0.0.1';
-const port = 3000;
+const app = express();
+
+app.use(function (request, reponse, next) {
+  reponse.writeHead(200, { 'Content-Type': 'text/plain'});
+  next();
+});
 
 
-
+// Database set up
 let db = new sqlite3.Database('A2.db', sqlite3.OPEN_READONLY, (err) => {
   if (err) {
     return console.error(err.message);
@@ -12,25 +18,47 @@ let db = new sqlite3.Database('A2.db', sqlite3.OPEN_READONLY, (err) => {
   console.log('Connected to Assignment 2 DB');
 });
 
-let query = 'SELECT * FROM Users';
 
-db.all(query, [], (err, rows) => {
-  if (err) {
-    throw err;
-  }
-  rows.forEach((row) => {
-    console.log(row);
-  });
+
+app.get('/users', function (request, reponse) {
+
+
+  db.all('SELECT * FROM Users', [], (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    console.log(rows);
+    var myJson = JSON.stringify(rows);
+    reponse.write(myJson);
+    reponse.end();
+   });
+
+  db.close();
+  //reponse.end();
+
 });
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+
+app.get('/messages', function (request, reponse) {
+  reponse.end("Messages table");
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+
+app.get('/likes', function (request, reponse) {
+  reponse.end("Likes table");
 });
+
+
+app.get('/contacts', function (request, reponse) {
+  reponse.end("Contacts table");
+});
+
+
+app.get('*', function (request, reponse) {
+  reponse.end('404 page not found')
+});
+
+
+http.createServer(app).listen(3000)
 
 //http://evanhahn.com/understanding-express/
